c5449fe675a86c312bc12c9ed70e5be8b95602e0
siv <subhashpadala18@gmail.com>
2023-12-21 16:26:59 +0530

    update base-url

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "siv-x-siv.vercel.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
